d5daf20bbb9670bc850dd3fef73ce161a8aecaf6
Breno <brenorodrigues0312@gmail.com>
2023-07-01 13:26:53 -0300

    ATTjs

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_bjae46m";
+    const templateID = "template_nd29j6n";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Sua menssagem foi enviada! Aguarde sua reposta.");
+        })
+        .catch((err) => console.log(err));
+}

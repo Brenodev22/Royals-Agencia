377d33ea33041d99dc984df6016e325c7890b466
Breno <brenorodrigues0312@gmail.com>
2023-07-24 16:58:05 -0300

    scrollAnimated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Preloader
 
-var loader = document.getElementById("preloader");
+//var loader = document.getElementById("preloader");
 
-window.addEventListener("load", function () {
-    loader.style.display = "none";
-});
+//window.addEventListener("load", function () {
+//    loader.style.display = "none";//
+//});
 
 // Send Emails
 
@@ -27,4 +27,44 @@ function sendMail() {
             console.log(res);
         })
         .catch((err) => console.log(err));
+}
+
+const debounce = function (func, wait, immediate) {
+    let timeout;
+    return function (...args) {
+        const context = this;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+    target.forEach(function (element) {
+        if (windowTop > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    });
+}
+
+animeScroll();
+
+if (target.length) {
+    window.addEventListener(
+        "scroll",
+        debounce(function () {
+            animeScroll();
+        }, 150)
+    );
 }
